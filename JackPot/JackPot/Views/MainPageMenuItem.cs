d25620775bd1d20fe5500060e5429bed1f9b3353
surajmca <surajmajumdar786@gmail.com>
2019-07-04 18:36:25 +0530

    done

--- JackPot/JackPot/Views/MainPageMenuItem.cs
+++ JackPot/JackPot/Views/MainPageMenuItem.cs
@@ -9,53 +9,12 @@ namespace JackPot.Views
 
     public class MainPageMenuItem
     {
-          public int Id { get; set; }
+        public int Id { get; set; }
         public string Title { get; set; }
-
         public Type TargetType { get; set; }
         public MainPageMenuItem()
         {
-            if(Id==0)
-            {
-                TargetType = typeof(Order);
-            }
-            if (Id == 1)
-            {
-                TargetType = typeof(PreviousTRX);
-            }
-            if (Id == 2)
-            {
-                TargetType = typeof(SearchReceipt);
-            }
-            if (Id == 3)
-            {
-                TargetType = typeof(Order);
-            }
-            if (Id == 4)
-            {
-                TargetType = typeof(Order);
-            }
-            if (Id == 5)
-            {
-                TargetType = typeof(Order);
-            }
-            if (Id == 6)
-            {
-                TargetType = typeof(Order);
-            }
-            if (Id == 7)
-            {
-                TargetType = typeof(CustomerDetail);
-            }
-            if (Id == 8)
-            {
-                TargetType = typeof(Order);
-            }
-            if (Id == 9)
-            {
-                TargetType = typeof(LogIn);
-            }
-
+           
         }
 
       
