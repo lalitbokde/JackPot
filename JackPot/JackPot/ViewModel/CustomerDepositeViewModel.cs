7293faa47e32d4343741d7fd9eaa18bfacb6c3dd
surajmca <surajmajumdar786@gmail.com>
2019-07-09 11:49:43 +0530

    done

---
+++ JackPot/JackPot/ViewModel/CustomerDepositeViewModel.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace JackPot.ViewModel
+{
+    public class CustomerDepositeViewModel: INotifyPropertyChanged
+    {
+
+        ICommand btn_Close;
+        ICommand btn_Deposite;
+        public ICommand btnDeposite =>
+        btn_Deposite ?? (btn_Deposite = new Command(async () => DepositeAsync()));
+
+        string customer;
+        public string Customer
+        {
+            get { return customer; }
+            set
+            {
+                if (customer != value)
+                {
+                    customer = value;
+                    OnPropertyChanged(nameof(Customer));
+
+                }
+            }
+        }
+
+        string customerName;
+        public string CustomerName
+        {
+            get { return customerName; }
+            set
+            {
+                if (customerName != value)
+                {
+                    customerName = value;
+                    OnPropertyChanged(nameof(CustomerName));
+
+                }
+            }
+        }
+
+        string deposite;
+        public string Deposite
+        {
+            get { return deposite; }
+            set
+            {
+                if (deposite != value)
+                {
+                    deposite = value;
+                    OnPropertyChanged(nameof(Deposite));
+
+                }
+            }
+        }
+
+
+
+        public void Close()
+        {
+        }
+
+        public void DepositeAsync()
+        {
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+    }
+}
---
+++ JackPot/JackPot/ViewModel/PayWinnerViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace JackPot.ViewModel
+{
+   public class PayWinnerViewModel : INotifyPropertyChanged
+    {
+        ICommand btn_ShowData;
+        ICommand btn_PayWinner;
+        ICommand btn_Close;
+
+        public ICommand btnShowData =>
+       btn_ShowData ?? (btn_ShowData = new Command(async () => ShowData()));
+
+        public ICommand PayWinner =>
+    btn_PayWinner ?? (btn_PayWinner = new Command(async () => ShowData()));
+
+        public ICommand btnClose =>
+           btn_Close ?? (btn_Close = new Command(async () => ShowData()));
+
+
+
+        string enterReceipt;
+        public string EnterReceipt
+        {
+            get { return enterReceipt; }
+            
+            set
+            {
+                if (enterReceipt != value)
+                {
+                    enterReceipt = value;
+                    OnPropertyChanged(nameof(EnterReceipt));
+
+                }
+            }
+        }
+
+        string _totalAmt;
+        public string TotalAmt
+        {
+            get { return _totalAmt; }
+            set
+            {
+                if (_totalAmt != value)
+                {
+                    _totalAmt = value;
+                    OnPropertyChanged(nameof(TotalAmt));
+
+                }
+            }
+        }
+
+
+        public void ShowData()
+        {
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void OnPropertyChanged([CallerMemberName]string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+
+    }
+}
